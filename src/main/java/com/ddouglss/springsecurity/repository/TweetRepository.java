b2dfaf0a9733a40f4f5c9add188c5712113174d0
ddouglss <ddouglss1999@gmail.com>
2025-08-04 20:28:46 -0300

    feat: criando os repository de User, Role e Tweet

---
+++ src/main/java/com/ddouglss/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package com.ddouglss.springsecurity.repository;
+
+import com.ddouglss.springsecurity.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+}
