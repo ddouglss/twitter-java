0fdf9fc267470079d916bd638afc51eac64a84a9
ddouglss <ddouglss1999@gmail.com>
2025-08-04 20:21:59 -0300

    feat: criando os perfil de basic, admin e implementando as entidades

--- src/main/java/com/ddouglss/springsecurity/entities/Tweet.java
+++ src/main/java/com/ddouglss/springsecurity/entities/Tweet.java
@@ -6,11 +6,12 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.Instant;
 
 @Entity
-@Table(name = "tb_tweet")
+@Table(name = "tb_tweets")
 public class Tweet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
     private Long tweetId;
 
     private User user;
@@ -19,4 +20,36 @@ public class Tweet {
 
     @CreationTimestamp
     private Instant creationTimestamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
 }
--- src/main/java/com/ddouglss/springsecurity/entities/User.java
+++ src/main/java/com/ddouglss/springsecurity/entities/User.java
@@ -1,9 +1,6 @@
 package com.ddouglss.springsecurity.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 
 import java.util.Set;
@@ -13,13 +10,55 @@ import java.util.UUID;
 @Table(name = "tb_users")
 public class User {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID userId;
 
+    @Column(unique = true)
     private String userName;
 
     private String password;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
 
 }
