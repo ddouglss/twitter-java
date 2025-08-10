0c54b2f571dd68f7a7abb09b299e05f7a351b31e
ddouglss <ddouglss1999@gmail.com>
2025-08-10 00:35:06 -0300

    feat: configurando a chave publica e privada

--- src/main/java/com/ddouglss/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/ddouglss/springsecurity/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.ddouglss.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.public-key}")
+    private RSAPublicKey publicKey;
+    @Value("${jwt.private-key}")
+    private RSAPrivateKey privateKey;
 
     @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return http.build();
     }
+
 }
