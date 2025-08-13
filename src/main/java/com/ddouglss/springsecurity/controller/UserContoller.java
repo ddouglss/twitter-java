2b7f82cc1a19d59c1344ffe0a99cb356c9e4cd28
ddouglss <ddouglss1999@gmail.com>
2025-08-13 01:54:52 -0300

    feat: criando funcionalidade de criar um novo usuário

---
+++ src/main/java/com/ddouglss/springsecurity/controller/UserContoller.java
@@ -0,0 +1,52 @@
+package com.ddouglss.springsecurity.controller;
+
+import com.ddouglss.springsecurity.data.dto.CreateUserDto;
+import com.ddouglss.springsecurity.entities.Role;
+import com.ddouglss.springsecurity.entities.User;
+import com.ddouglss.springsecurity.repository.RoleRepository;
+import com.ddouglss.springsecurity.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserContoller {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserContoller(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto) {
+
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+
+        var userFromDb = userRepository.findByUserName(dto.userName());
+        if (userFromDb.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        var user = new User();
+        user.setUserName(dto.userName());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/ddouglss/springsecurity/data/dto/CreateUserDto.java
@@ -0,0 +1,4 @@
+package com.ddouglss.springsecurity.data.dto;
+
+public record CreateUserDto (String userName, String password) {
+}
